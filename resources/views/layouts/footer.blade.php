931b31634f471f172b53caf3e07adc9596d43912
John Liton Mardy <mardy.cse@gmail.com>
2025-06-26 18:02:28 +0600

    fix footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
- <footer id="siteFooter" class="flex w-full justify-between items-center bg-white shadow-lg absolute right-0 border-t border-[#EBEEF2] h-[60px] pl-0 sm:pl-[80px] lg:pl-[270px]">
+ <footer id="siteFooter" class="flex w-full justify-between items-center bg-white shadow-lg fixed bottom-0 right-0 border-t border-[#EBEEF2] h-[60px] pl-0 sm:pl-[80px] lg:pl-[270px] z-10">
       <div class="flex w-full items-center justify-center text-center p-4">
-        <a class="text-[13px] sm:text-[15px] text-[#000B8C]" href="https://ba-systems.com/" target="_blank">
-          Copyright © Shakir Ahmed Shuvo. 2025
-        </a>
+        <span class="text-[13px] sm:text-[15px] text-[#000B8C]">
+          Copyright © Amar School 2025
+        </span>
       </div>
     </footer>
